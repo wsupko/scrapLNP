dc460ae5840327e7f006b439ccd0d3edc6cf8c73
wsupko <supekw@gmail.com>
2018-02-18 22:53:23 +0100

    Change to ESA

--- scrapLNP_rawCode.R
+++ scrapLNP_rawCode.R
@@ -1,23 +1,27 @@
 library(rvest)
 library(data.table)
 
-kol <- 1
-mainLink <- paste('https://www.laczynaspilka.pl/rozgrywki/ii-liga,3.html?round=', kol, sep = '')
-
-main <- read_html(mainLink)
-
-mainLinks <- main %>% html_nodes('a.action') %>% html_attr('href')
-
-lnk <- mainLinks[1]
-
+gkol <- 1:22
 gameData <- data.table()
-for (lnk in mainLinks){
+
+for (kol in gkol){
+    mainLink <- paste('https://www.laczynaspilka.pl/rozgrywki/ekstraklasa,1.html?round=', kol, sep = '')
     
-    gameCode <- read_html(lnk)
-    gosp <- gameCode %>% html_nodes("span.team-name.left") %>% html_text(trim = TRUE)
-    gosc <- gameCode %>% html_nodes("span.team-name.right") %>% html_text(trim = TRUE)
-    frek <- gameCode %>% html_node('div.toggle-content.report-info-content') %>% html_text(trim = TRUE)
+    main <- read_html(mainLink)
     
-    gameData <- rbind(gameData, data.table(lnk, gosp, gosc, frek))
+    mainLinks <- main %>% html_nodes('a.action') %>% html_attr('href')
+    
+    for (lnk in mainLinks){
+        
+        gameCode <- read_html(lnk)
+        gosp <- gameCode %>% html_nodes("span.team-name.left") %>% html_text(trim = TRUE)
+        gosc <- gameCode %>% html_nodes("span.team-name.right") %>% html_text(trim = TRUE)
+        frek <- gameCode %>% html_node('div.toggle-content.report-info-content') %>% html_text(trim = TRUE)
+        
+        gameData <- rbind(gameData, data.table(kol, lnk, gosp, gosc, frek))
+    }
 }
 
+gameData[, nFrek:= str_extract(frek, '([0-9])+')]
+
+write.csv(gameData, file = './data/out/Frek.csv', row.names = F)
